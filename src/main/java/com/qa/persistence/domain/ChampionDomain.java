0c450cd96dac498bb3dbbe3fe4b109724baf7dc9
RGlossop <RGlossop@qa.com>
2021-01-25 10:27:35 +0000

    LOL-15 LOL-16 added champion domain/dto functionality

--- src/main/java/com/qa/persistence/domain/ChampionDomain.java
+++ src/main/java/com/qa/persistence/domain/ChampionDomain.java
@@ -1,8 +1,66 @@
 package com.qa.persistence.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class ChampionDomain {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String name;
+	@NotNull
+	private String role;
+
+	public ChampionDomain() {
+		super();
+	}
+
+	public ChampionDomain(String name, String role) {
+		super();
+		this.name = name;
+		this.role = role;
+	}
+
+	public ChampionDomain(Long id, String name, String role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "ChampionDomain [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+
 }
