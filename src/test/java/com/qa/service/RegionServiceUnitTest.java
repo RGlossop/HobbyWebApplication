2630c0999ddad3652da211484c2312f8b5acad2c
RGlossop <RGlossop@qa.com>
2021-01-29 10:28:00 +0000

    LOL-18 LOL-19 Full champion service and controller integration and unit tests

--- src/test/java/com/qa/service/RegionServiceUnitTest.java
+++ src/test/java/com/qa/service/RegionServiceUnitTest.java
@@ -1,5 +1,22 @@
 package com.qa.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.HobbyWebApplication;
+
+@SpringBootTest(classes = HobbyWebApplication.class)
 public class RegionServiceUnitTest {
 
+	@Autowired
+	private RegionService service;
+	
+//	@MockBean
+//	private RegionRepo repoMock;
+//	
+//	@MockBean
+//	private ModelMapper mapper;
+	
+	
 }
